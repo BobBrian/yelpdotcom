96c813a7e1f02e408a89c6f1588362d8005f6d77
BobBrian <rellrocker235@gmail.com>
2023-10-16 12:48:16 -0400

    hook commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+//This is the version of the main code that will be pushed into the master branch
+//The code here uses remenant code from the static project file
+
+//This page is where the listAllResturants and the Link to createReview and reviewDetails Page
+//Stage 1 is the code built with dummy data to populate fields and make it easier to design
+//The Front End
+//VERSION 1:USING DUMMY DATA
+//Dave Grey Video:https://www.youtube.com/watch?v=2NEV_M7NN6k
+//ByteGrad Video:https://www.youtube.com/watch?v=TPACABQTHvM&t=84s
+
 export default function Home() {
   return (
     <div>Home</div>
