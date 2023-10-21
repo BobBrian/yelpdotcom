91b214a829b8f9e2a9766760e72bad2608a86f65
BobBrian <rellrocker235@gmail.com>
2023-10-21 16:33:02 -0400

    routing fixes

--- app/review/[id]/page.tsx
+++ app/review/[id]/page.tsx
@@ -25,7 +25,7 @@ export default async function ReviewDetails({params: {id}}: Params) {
                 <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium 
                 text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 
                 focus:outline-none focus:ring-blue-300">
-                    <Link href="/table">Back</Link>   
+                    <Link href="/">Back</Link>   
                 </a>
         </div>
         </>
